db5759fdb84012f0a1828100a08af89f13053306
Akash Gautam <akash.gautam2024@nst.rishihood.edu.in>
2025-05-04 20:46:11 +0530

    Complete Home Page and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import OurPolicies from '../components/OurPolicy';
+import NewsLetterBox from '../components/NewsLetterBox';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <OurPolicies/>
+      <NewsLetterBox/>
+      <Footer/>
     </div>
   )
 }
