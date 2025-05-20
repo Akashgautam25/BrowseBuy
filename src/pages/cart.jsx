2868e472c4f667943c8f5de82ab6932899fab1ac
Akash Gautam <akash.gautam2024@nst.rishihood.edu.in>
2025-05-20 11:16:59 +0530

    Update cart.jsx

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer';
 
 const Orders = () => {
   return (
     <div>
-      
+      <h2 className='text-8xl text-center  text-black'>Under</h2>
+      <h2 className='text-8xl text-center  text-black'>Construction</h2>
+      <Footer/>
     </div>
   )
 }
