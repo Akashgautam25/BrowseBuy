6cd308deac1e00076e7fb688fdde8e34692ac398
Akash Gautam <akash.gautam2024@nst.rishihood.edu.in>
2025-06-16 15:34:43 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD43C3qepfkO8gGiOT4jmtEV1W3fSjyObw",
   authDomain: "e-commerce-a6b67.firebaseapp.com",
   projectId: "e-commerce-a6b67",
-  storageBucket: "e-commerce-a6b67.appspot.com", // ✅ Corrected here
+  storageBucket: "e-commerce-a6b67.appspot.com",
   messagingSenderId: "348808944658",
   appId: "1:348808944658:web:5ad31f2ea00a0578040ccb",
   measurementId: "G-SP2TQ2QK0S"
